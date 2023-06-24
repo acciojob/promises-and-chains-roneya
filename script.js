e36bc22765683969c0f22773afaa312e2226dfa4
Rohan Vidhate <49265832+roneya@users.noreply.github.com>
2023-06-24 13:35:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
-//your JS code here. If required.
+ const form = document.querySelector('#myForm');
+  const nameInput = document.querySelector('#name');
+  const ageInput = document.querySelector('#age');
+  const btn = document.querySelector('#btn');
+
+  form.addEventListener('submit', (event) => {
+    event.preventDefault(); // Prevent form from submitting normally
+
+    if (nameInput.value === '' || ageInput.value === '') {
+      alert('Please fill out all fields.'); // Show validation error
+      return;
+    }
+
+    const age = parseInt(ageInput.value);
+
+    const promise = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (age >= 18) {
+          resolve();
+        } else {
+          reject();
+        }
+      }, 4000);
+    });
+
+    promise
+      .then(() => {
+        alert(`Welcome, ${nameInput.value}. You can vote.`);
+      })
+      .catch(() => {
+        alert(`Oh sorry ${nameInput.value}. You aren't old enough.`);
+      });
+  });
+
+
